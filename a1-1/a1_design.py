ce17b39fe7d493b21e98a9344e7dfa7a9c6326c2
fazilz <zuhaibfazil@gmail.com>
2016-02-07 15:42:23 -0500

    added the row and the matrix clas

--- a1-1/a1_design.py
+++ a1-1/a1_design.py
@@ -60,23 +60,49 @@ class Matrix():
         '''(Matrix, int, int)'''
         pass
 
-    def get_value(self, n_row,):
-        '''(Column, int) -> int or str
-        Get the value stored at position n in the row list
+    def get_value(self, n_row, n_col):
+        '''(Matrix, int, int) -> int or str
+        Get the value stored at position n in the Matrix
         '''
         pass
 
-    def set_value(self, n, item):
-        '''(Row, int, int or str) -> NoneType
-        Change the value stored at position n to item
-        REQ position n MUST exist in the the row and it must store an int or
-        a str
+    def set_value(self, n_row, n_col, item):
+        '''(Matrix, int, int, int or str) -> NoneType
+        Change the value stored at position n row and n column to item
+        REQ position n MUST exist in the the row and column and it must store
+        an int or a str
         '''
         pass
 
     def add_value(self, item):
-        '''(Row, int or str) -> NoneType
-        Add item to the end of the Row
+        '''(Matrix, int or str) -> NoneType
+        Add item to the end of the Matrix
+        '''
+        pass
+
+
+    def add(self, r1, c1, r2, c2):
+        '''(Matrix, int, int, int, int) -> int
+        Add values from (r1, c1) + (r2, c2)
+        where (r1,c1) resolves to a single value, since they are are the same
+        thing.
+        '''
+        pass
+
+    def mult(self, r1, c1, r2, c2):
+        '''(Matrix, int, int, int, int) -> int
+        Do this: (r1, c1) * (r2, c2)
+        where (r1,c1) resolves to a single value, since they are are the same
+        thing.
+        '''
+        pass
+
+
+    def diff(self, r1, c1, r2, c2):
+        '''(Matrix, int, int, int, int) -> int
+        Do this: (r1, c1) - (r2, c2)
+        where (r1,c1) resolves to a single value, since they are are the same
+        thing.
         '''
         pass
 
